3f3c4cf74934a40ebad33865a5f0231075e6152c
Tvde1 <tim3345@live.nl>
2017-06-20 16:14:30 +0200

    added controller

---
+++ VerkiezingHelper/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using VerkiezingHelper.Models;
+
+namespace VerkiezingHelper.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Open()
+        {
+            var openModel = new OpenModel();
+            return null;
+        }
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
